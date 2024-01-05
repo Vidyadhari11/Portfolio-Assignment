59ed825a41fc8cf990d693974ab2bdbd5d207bc6
Vidyadhari <vidyadharibelagal@gmail.com>
2024-01-05 12:07:35 +0530

    changes in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,30 +4,29 @@ import './index.css'
 
 const Header = props => {
     return(
-        <div className="nav-header">
-            <h1 className="name">Vidyadhari Belagal</h1>
-            <ul className="nav-menu">
-                <li className="nav-menu-item">
-                  <Link to="/" className="nav-link">
-                    About
-                  </Link>
-                </li>
-    
-                <li className="nav-menu-item">
-                  <Link to="/projects" className="nav-link">
-                    Projects
-                  </Link>
-                </li>
-    
-                <li className="nav-menu-item">
-                  <Link to="/contact" className="nav-link">
-                    Contacts
-                  
-                  </Link>
-                </li>
-              </ul>
-            </div>
+      <div className="nav-header">
+      <h1 className="name">Vidyadhari Belagal</h1>
+      <ul className="nav-menu">
+          <li className="nav-menu-item">
+            <Link to="/" className="nav-link">
+              About
+            </Link>
+          </li>
       
+          <li className="nav-menu-item">
+            <Link to="/projects" className="nav-link">
+              Projects
+            </Link>
+          </li>
+      
+          <li className="nav-menu-item">
+            <Link to="/contact" className="nav-link">
+              Contacts
+            
+            </Link>
+          </li>
+        </ul>
+      </div>
     )
 
 }
